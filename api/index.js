996929352f1633bf9c953381895f4b6a87353b8e
Florian Kretlow <floriankretlow@gmx.de>
2021-01-05 13:17:51 +0100

    flesh out api including the actual data request

--- api/index.js
+++ api/index.js
@@ -1,19 +1,34 @@
+const OBD_BASE_URL = "https://www.oekobaudat.de/OEKOBAU.DAT/resource/";
+
+// bent is a lightweight modern request library
+const bent = require("bent");
+const getString = bent("string");
+
 module.exports = async (req, res) => {
-  let { headers, method } = req;
+    let { method } = req;
 
-  if (method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.status(200).send();
-  }
+    // CORS preflight
+    if (method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.status(200).send();
+    }
 
-  else {
-    console.log(req.query);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Vary", "origin");
-    res.setHeader("Content-Type", "text/html");
+    // the actual request
+    else {
+        // parse the query (need to use ; instead of & in the original request to the api)
+        let db_query = req.query.query.replace(/;/g, "&");
+        console.log(`query: ${db_query}`);
 
-    console.log("sending response");
-    res.status(200).send(`Hello from the server. You accessed this URL: ${req.url}`);
-  }
+        // request
+        getString(OBD_BASE_URL + db_query)
+            .then(body => {
+                console.log("received data:\n", body);
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Vary", "origin");
+                // res.setHeader("Content-Type", "text/html");
+                res.status(200).send(body);
+            })
+            .catch(e => console.error(e));
+    }
 }
